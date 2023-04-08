2344aab79bdd5b5e0abd91629c9167a018bd593a
Radek <rj.airoo@gmail.com>
2023-04-08 14:53:41 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,30 @@
+const x = 50
+const y = 30
+
+if (x > y) {
+	console.log(`${x} jest większe niż ${y}.`)
+}
+
+//--------------------------
+
+const color = 'blue'
+const newColor = 'green'
+
+if (color === newColor) {
+	console.log('Kolory się zgadzają.')
+} else {
+	console.log('Kolory się nie zgadzają.')
+}
+
+//---------------------------
+
+const v = 100
+const z = 50
+
+if (v > z) {
+	console.log(`v > z`);
+} else if (v === z) {
+	console.log(`v = z`);
+} else {
+	console.log(`v < z`);
+}
